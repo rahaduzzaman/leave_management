cf83ea48751b6bc259f9b9ec8cabdba4833c1d23
Md. Rahaduzzaman <rahadu973@gmail.com>
2016-02-11 02:40:59 +0600

    primary Mock up

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Leave management</title>
+
+        <!-- Bootstrap -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="assets/css/scrolling-nav.css" rel="stylesheet">    
+        <link href="assets/css/app.css" rel="stylesheet">
+        <link href="assets/css/style.css" rel="stylesheet">
+
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header page-scroll">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="#page-top">LM</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                        <li class="hidden">
+                            <a class="page-scroll" href="#page-top"></a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#about">About</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#services">Services</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Intro Section -->
+        <section id="intro" class="intro-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">Slider</div>
+                    <div class="col-md-4">
+                        <form class="">
+                            <div class="form-group">
+                                <input type="text" class="form-control input-lg" placeholder="name">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control input-lg" placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <select class="form-control" >
+                                    <option class="disabled" >Select User type</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="teacher">Teacher</option>
+                                    <option value="student">Student</option>
+                                </select>
+
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary btn-lg btn-block">Sign In</button>
+                                <span><a href="#">Not registered?</a></span>
+                                <span class="pull-right"><a href="#">Please Register here</a></span>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+
+
+        <footer class="footer">
+            <div class="container">
+                <p class="text-ash-lt text-center">Created By us</p>
+            </div>
+        </footer>
+
+
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+
+        <!-- backstretch JavaScript -->
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+
+        <!-- Scrolling Nav JavaScript -->
+        <script src="assets/js/jquery.easing.min.js"></script>
+        <script src="assets/js/scrolling-nav.js"></script>
+        <script src="assets/js/script.js"></script>
+
+
+    </body>
+</html>
---
+++ view/admin/admin_index.php
@@ -0,0 +1,115 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Bootstrap 101 Template</title>
+
+        <!-- Bootstrap -->
+        <link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../../assets/css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="../../assets/css/scrolling-nav.css" rel="stylesheet">    
+        <link href="../../assets/css/app.css" rel="stylesheet">
+        <link href="../../assets/css/style.css" rel="stylesheet">
+
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header page-scroll">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="#page-top">LM</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                        <li class="hidden">
+                            <a class="page-scroll" href="#page-top"></a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#">Hi <span class="text-green-lt"> Admin</span></a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#services"><span class="text-danger">Logout</span></a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a class="page-scroll" href="#">Home</a></li>
+                        <li><a class="page-scroll" href="#">Add Student</a></li>
+                        <li><a class="page-scroll" href="#">Add Teacher</a></li>
+                        <li><a class="page-scroll" href="#">Assign Teacher</a></li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Intro Section -->
+        <section id="intro" class="intro-section">
+            <div class="container">
+                Admin Details
+            </div>
+
+        </section>
+
+        <!--         About Section 
+                <section id="about" class="about-section">
+        
+                </section>
+        
+                 Services Section 
+                <section id="services" class="services-section">
+        
+                </div>
+            </section>
+        
+             Contact Section 
+            <section id="contact" class="contact-section">
+        
+            </section>-->
+         
+        
+        <footer class="footer">
+            <div class="bg-glow">
+                <p class="text-ash-lt text-center">Created By us</p>
+            </div>
+        </footer>
+        
+        <!-- jQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="../../assets/js/bootstrap.min.js"></script>
+
+        <!-- Scrolling Nav JavaScript -->
+        <script src="../../assets/js/jquery.easing.min.js"></script>
+        <script src="../../assets/js/scrolling-nav.js"></script>
+        <script src="../../assets/js/script.js"></script>
+
+    </body>
+</html>
+
